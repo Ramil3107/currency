afba5b9493dc4b4bd6a975d7a57eb70ab20a149f
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-11-04 18:12:01 +0300

    add styles for app and currencyinput

--- src/CurrencyInput.js
+++ src/CurrencyInput.js
@@ -1,14 +1,17 @@
+import styles from "./CurrencyInput.module.css"
 
 export const CurrencyInput = ({ rates, amount, currency, onChangeAmount, onChangeCurrency }) => {
     return (
-        <div>
+        <div className={styles.currencyInput}>
             <input
+                className={styles.amount}
                 type="number"
                 value={amount}
                 onChange={(e) => onChangeAmount(e.target.value >= 0 ? e.target.value : "")}
                 disabled={!rates}
             />
             <select
+                className={styles.currency}
                 disabled={!rates}
                 value={currency}
                 onChange={(e) => onChangeCurrency(e.target.value)}
